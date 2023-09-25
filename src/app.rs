2273143330965037e01da79c4ba014ab86c0c0c1
ChihYing <spacetime0311@gmail.com>
2023-09-25 14:00:10 +0800

    feat: layout the frontend structure

--- src/app.rs
+++ src/app.rs
@@ -15,28 +15,8 @@ pub fn App(cx: Scope) -> impl IntoView {
         // sets the document title
         <Title text="Welcome to Leptos"/>
 
-        // content for this welcome page
-        <Router>
-            <main>
-                <Routes>
-                    <Route path="" view=HomePage/>
-                    <Route path="/*any" view=NotFound/>
-                </Routes>
-            </main>
-        </Router>
-    }
-}
-
-/// Renders the home page of your application.
-#[component]
-fn HomePage(cx: Scope) -> impl IntoView {
-    // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(cx, 0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
-
-    view! { cx,
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        <MessageHistory/>
+        <MessageInputField/>
     }
 }
 
