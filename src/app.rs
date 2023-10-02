02cec707dd6e291af166fb5755494d4d390e6744
ChihYing <spacetime0311@gmail.com>
2023-10-02 18:02:19 +0800

    feat: Create affect

--- src/app.rs
+++ src/app.rs
@@ -2,6 +2,7 @@ use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
 
+use crate::api::converse;
 use crate::model::conversation::{Conversation, Message};
 
 #[component]
@@ -18,7 +19,28 @@ pub fn App() -> impl IntoView {
         };
 
         set_conversation.update(move |c| c.messages.push(user_message));
-        async move { todo!() }
+        async move { converse(conversation()).await }
+    });
+
+    create_effect(move |_| {
+        if let Some(_) = send.input().get() {
+            let model_message = Message {
+                text: String::from("..."),
+                user: false,
+            };
+
+            set_conversation.update(move |c| {
+                c.messages.push(model_message);
+            });
+        }
+    });
+
+    create_effect(move |_| {
+        if let Some(Ok(response)) = send.value().get() {
+            set_conversation.update(move |c| {
+                c.messages.last_mut().unwrap().text = response;
+            });
+        }
     });
 
     view! {
