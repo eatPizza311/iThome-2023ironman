e761ad385c170934d063128285d4a1ff325fcb67
ChihYing <spacetime0311@gmail.com>
2023-10-03 20:38:10 +0800

    feat: Add tailwind css style

--- src/app/components/history.rs
+++ src/app/components/history.rs
@@ -2,8 +2,10 @@ use leptos::{html::Div, *};
 
 use crate::model::conversation::Conversation;
 
-const USER_MESSAGE_CLASS: &str = "max-w-md p-4 mb-5 rounded-lg self-end bg-blue-500 text-white";
-const MODEL_MESSAGE_CLASS: &str = "max-w-md p-4 mb-5 rounded-lg self-start bg-gray-200 text-black";
+const USER_MESSAGE_CLASS: &str = "message user bg-emerald-300 border-emerald-300 self-end";
+const MODEL_MESSAGE_CLASS: &str = "message assistant bg-blue-100 border-blue-100 self-start";
+const DOT_3_CLASS: &str =
+    "rounded-lg py-2.5 px-6 mb-4 message assistant bg-blue-100 border-blue-100 self-start";
 
 #[component]
 pub fn MessageHistory(conversation: ReadSignal<Conversation>) -> impl IntoView {
@@ -20,10 +22,21 @@ pub fn MessageHistory(conversation: ReadSignal<Conversation>) -> impl IntoView {
         <div class="h-screen pb-24 w-full flex flex-col overflow-y-auto p-5 border-gray-300 bg-gray-100" node_ref=chat_div_ref>
             {move || conversation.get().messages.iter().map(move |message| {
                 let class_str = if message.user { USER_MESSAGE_CLASS } else { MODEL_MESSAGE_CLASS };
-                view! {
-                    <div class={class_str}>
-                        {message.text.clone()}
-                    </div>
+                // ... wave while waiting for response
+                if message.text == String::from("...") {
+                    view! {
+                        <div class=DOT_3_CLASS>
+                            <div class="type-indicator">
+                                <span>.</span><span>.</span><span>.</span>
+                            </div>
+                        </div>
+                    }
+                } else {
+                    view! {
+                        <div class=class_str>
+                            {message.text.clone()}
+                        </div>
+                    }
                 }
             }).collect::<Vec<_>>()
             }
--- src/app/components/input.rs
+++ src/app/components/input.rs
@@ -12,7 +12,7 @@ pub fn MessageInputField(send: Action<String, Result<String, ServerFnError>>) ->
                 send.dispatch(input.value());
                 input.set_value("")
             }>
-                <input type="text" class="w-2/3 p-4 border rounded-full input-field border-gray-300 text-black" node_ref=input_ref/>
+                <input type="text" class="w-2/3 p-4 border rounded-full input-field border-gray-300 text-black" placeholder="Say something..." node_ref=input_ref/>
                 <input type="submit" class="h-full p-4 rounded-full cursor-pointer bg-blue-500 text-white" />
             </form>
         </div>
